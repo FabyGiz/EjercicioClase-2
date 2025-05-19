0d7893b355d04b1ade5c13b739271e93fa5f0070
fabym <fabym@ARTURO-PC>
2025-05-19 14:36:46 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Entities;
+using System.Data.Common;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Instancia del gato
+
+        var myCat = new Cat() { Id = 1, Name="Misingo" };
+
+        Console.WriteLine("Id del gato: "+ myCat.Family);
+
+        Console.WriteLine("Nombre del gato: "+ myCat.Name);
+
+        Console.WriteLine("Ingrese una descripcion para el gato");
+
+         myCat.Description=Console.ReadLine();
+    }
+}
---
+++ Entities/Cat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Cat : Animal //hereda de animal. En caso de querer usar una interfaz se pone , y el nombre de la interfaz
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+
+        public Cat() { }
+
+
+
+    }
+}
